5241077e09585cdc9c851eb34b0404762e7dc3b2
yrajput <yuvrajrathore142@gmail.com>
2025-08-22 15:35:40 +0530

    hero section complete

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import heroIamge01 from '../assets/images/hero-img01.png'
+import heroIamge02 from '../assets/images/hero-img02.png'
+import heroIamge03 from '../assets/images/hero-img03.png'
 const Home = () => {
   return (
     // =======hero section========
@@ -15,6 +17,39 @@ const Home = () => {
                                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat nulla facilis unde officia repellendus? Voluptates quis sunt blanditiis voluptate totam, quos at quibusdam itaque neque obcaecati consequuntur recusandae culpa! Officiis?
                             </p>
                             <button className='btn'>Request An Appointment</button>
+                        </div>
+
+
+                        {/* ========hero-counter========= */}
+                        <div className='mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px] '>
+                            <div>
+                                <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>30+</h2>
+                                <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span>
+                                <p className='text-para'>Year of Experience</p>
+                            </div>
+
+                            <div>
+                                <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>15+</h2>
+                                <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span>
+                                <p className='text-para'>Clinic Location</p>
+                            </div>
+
+                            <div>
+                                <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>100%</h2>
+                                <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span>
+                                <p className='text-para'>Patient Satisfaction</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* ======hero-contant======= */}
+                    <div className='flex gap-[30px] justify-end'>
+                        <div>
+                            <img className='w-[full]' src={heroIamge01} alt="" />
+                        </div>
+                        <div className='mt-[30px]'>
+                            <img className='w-[full] mb-[30px]' src={heroIamge02} alt="" />
+                            <img className='w-[full] ' src={heroIamge03} alt="" />
                         </div>
                     </div>
                 </div>
