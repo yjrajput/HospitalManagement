ff1e6dc63f3906f6e878370534c4201b2645628c
yrajput <yuvrajrathore142@gmail.com>
2025-08-23 12:52:36 +0530

    freature section complete

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,10 +5,13 @@ import heroIamge03 from '../assets/images/hero-img03.png'
 import icon01 from '../assets/images/icon01.png'
 import icon02 from '../assets/images/icon02.png'
 import icon03 from '../assets/images/icon03.png'
+import videoIcon from '../assets/images/video-icon.png'
+import avtarImg from '../assets/images/avatar-icon.png'
 import { Link } from 'react-router-dom'
 import { BsArrowRight } from "react-icons/bs";
 import About from '../components/about/About'
 import ServiceList from '../components/services/ServiceList'
+import featureImg from '../assets/images/feature-img.png'
 const Home = () => {
   return (
     
@@ -150,6 +153,52 @@ const Home = () => {
 
 
         {/* ==========feature section======== */}
+        <section>
+            <div className='container'>
+                <div className='flex items-center justify-between flex-col lg:flex-row '>
+                    <div className='xl:w-[670px]'>
+                        <h2 className='heading'>
+                            Get virtural treatment <br /> anytime.
+                        </h2>
+
+                        <ul className='pl-4'>
+                            <li className='text-para'>1. Schedule the appoinment directly.</li>
+                            <li className='text-para'>2. Search for your physician here, and contact there office.</li>
+                            <li className='text-para'>3. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque soluta, quisquam tempora deleniti error harum eveniet unde </li>
+                        </ul>
+                        <Link to='/'>
+                            <button className='btn'>Lrean more</button>
+                        </Link>
+                    </div>
+
+                    <div className='relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0'>
+                        <img src={featureImg} className='w-3/4' alt="" />
+
+                        <div className='w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 le:pt-4 lg:pb-[26px] rounded-[10px]'>
+                        <div className='flex items-center justify-between'>
+                            <div className='flex items-center gap-[6px] lg:gap-3'>
+                                <p className='text-[10px] leading-[10px] le:text-[14px] lg:leading-5 text-headingColor font-[600]' >Tue, 25</p>
+                                <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400] '>10:00AM</p>
+                            </div>
+
+                            <span className='w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]'>
+                                <img src={videoIcon} alt="" />
+                            </span>
+                        </div>
+
+                        <div className='w-[65px] lg:w-[96px] bg-[#ccf0f3] py-1 px-2 lg:py-[10px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-[4px] text-iresBlurColor font-[500] mt-2 lg:mt-4 rounded-full'>
+                            Consulatation
+                        </div>
+
+                        <div className='flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px] '>
+                            <img src={avtarImg} alt="" />
+                            <h4 className='text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor'>jhon wick</h4>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </>
   )
 }
