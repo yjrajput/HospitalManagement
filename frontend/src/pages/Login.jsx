762d1be80d4a205272128eae48c60b7b22c5b52e
yrajput <yuvrajrathore142@gmail.com>
2025-08-25 15:53:21 +0530

    Contact us page is complete

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   return (
     <section className='px-5 lg:px-0'>
       <div className='w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10'>
-        <h3 className='text-headingColor text-[22px] leading-9 font-bold md-10 '>
+        <h3 className='text-headingColor text-[22px] leading-9 font-bold mb-10 '>
             Hello! <span className='text-primaryColor'>Welcome</span> Back 🎉
         </h3>
 
